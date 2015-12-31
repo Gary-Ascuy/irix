a846f101f6c4c90249de3a26ee33e4c6c845c8d1
Gary Ascuy <gary.ascuy@gmail.com>
2015-12-30 22:00:11 -0400

    Improving main menu

--- Assets/presentation/GuiControl.cs
+++ Assets/presentation/GuiControl.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 
 public class GuiControl : MonoBehaviour {
+  private bool options;
+
   void Start() {
+    options = false;
   }
 
   void Update() {
@@ -11,21 +14,48 @@ public class GuiControl : MonoBehaviour {
   void OnGUI() {
     var w = Screen.width;
     var h = Screen.height;
-
-    if (GUI.Button(new Rect(w - 20 - w / 6, h - 20 - h / 10, w / 6, h / 10), "Play")) {
-      Application.LoadLevel("level 1");
+    var fontSize = h / 10 - 44;
+    if (fontSize < 20) {
+      fontSize = 20;
     }
 
-    if (GUI.Button(new Rect(20, h - 20 - h / 10, w / 6, h / 10), "Exit")) {
-      Application.Quit();
+    var style = new GUIStyle(GUI.skin.GetStyle("button"));
+    style.fontSize = fontSize;
 
-      // workaround and does not works too xD
-      PlayerPrefs.Save();
-      var threads = System.Diagnostics.Process.GetCurrentProcess().Threads;
-      foreach (var thread in threads) {
-        thread.Dispose();
+    var styleLabel = new GUIStyle(GUI.skin.GetStyle("button"));
+    styleLabel.fontSize = fontSize;
+    styleLabel.fontStyle = FontStyle.Bold;
+    styleLabel.normal.textColor = Color.green;
+
+    if (options) {
+      GUI.Box(new Rect(0, 0, w, h), "");
+      GUILayout.Space = 10;
+      GUILayout.BeginVertical();
+      GUILayout.BeginArea(new Rect(w / 4, h / 4, w / 2, h - h / 4));
+
+      GUILayout.Label("OPTIONS", styleLabel);
+      if (GUILayout.Button("Return", style)) {
+        options = false;
       }
-      System.Diagnostics.Process.GetCurrentProcess().Kill();
+
+      GUILayout.Button("Audio", style);
+      GUILayout.Button("Video", style);
+      GUILayout.Button("Game", style);
+      GUILayout.EndArea();
+      GUILayout.EndVertical();
+    } else {
+      if (GUI.Button(new Rect(w - 20 - w / 6, 20, w / 6, h / 10), "Options", style)) {
+        options = true;
+      }
+
+      if (GUI.Button(new Rect(w - 20 - w / 6, h - 20 - h / 10, w / 6, h / 10), "Play", style)) {
+        Application.LoadLevel("level 1");
+      }
+
+      if (GUI.Button(new Rect(20, h - 20 - h / 10, w / 6, h / 10), "Exit", style)) {
+        PlayerPrefs.Save();
+        Application.Quit();
+      }
     }
   }
 }
